e396f3e9a9893ff0a243aa86f70823d0fc316cc8
lucassouza4 <lussilva1998@gmail.com>
2024-12-18 00:19:59 -0300

    feat: adicionando proteção de rota

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
-import UserView from '@/views/UserView.vue'
-import ClimbersView from '@/views/ClimbersView.vue'
-import RankingView from '@/views/RankingView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
+import UserView from '@/views/UserView.vue';
+import ClimbersView from '@/views/ClimbersView.vue';
+import RankingView from '@/views/RankingView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,18 +22,30 @@ const router = createRouter({
       path: '/user',
       name: 'user',
       component: UserView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/climbers',
       name: 'climbers',
       component: ClimbersView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/ranking',
       name: 'ranking',
       component: RankingView,
+      meta: { requiresAuth: true },
     },
   ],
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('token');
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'login' });
+  } else {
+    next();
+  }
+});
+
+export default router;
